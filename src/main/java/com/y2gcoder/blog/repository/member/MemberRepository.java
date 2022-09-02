77f09e83b2e5d32039d3df8170b273c3d2e15851
y2gcoder <y2gcoder@gmail.com>
2022-09-02 17:30:58 +0900

    MemberRepository existsByEmail 추가

--- src/main/java/com/y2gcoder/blog/repository/member/MemberRepository.java
+++ src/main/java/com/y2gcoder/blog/repository/member/MemberRepository.java
@@ -3,5 +3,7 @@ package com.y2gcoder.blog.repository.member;
 import com.y2gcoder.blog.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {}
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	boolean existsByEmail(String email);
+}
 
