0fa768e967ee7276b7e6bffc294c4b587b8d2381
y2gcoder <y2gcoder@gmail.com>
2022-09-07 14:58:41 +0900

    로컬환경이랑 테스트 환경 분리

--- src/test/java/com/y2gcoder/blog/BlogApplicationTests.java
+++ src/test/java/com/y2gcoder/blog/BlogApplicationTests.java
@@ -2,7 +2,9 @@ package com.y2gcoder.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootTest
 class BlogApplicationTests {
 
