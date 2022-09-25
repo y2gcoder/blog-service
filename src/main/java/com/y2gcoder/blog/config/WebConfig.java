7d994d2302bb0b1cea980903496d32bab0abf722
y2gcoder <y2gcoder@gmail.com>
2022-09-25 19:38:02 +0900

    CORS 추가

--- src/main/java/com/y2gcoder/blog/config/WebConfig.java
+++ src/main/java/com/y2gcoder/blog/config/WebConfig.java
@@ -1,9 +1,25 @@
 package com.y2gcoder.blog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+				.allowedOrigins("*")
+				.allowedMethods(
+						HttpMethod.GET.name(),
+						HttpMethod.POST.name(),
+						HttpMethod.PUT.name(),
+						HttpMethod.PATCH.name(),
+						HttpMethod.DELETE.name(),
+						HttpMethod.OPTIONS.name()
+				);
+	}
 }
